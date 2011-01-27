5b2fefcb14cb8e7a975622e5c6b5119c09736833
Pavel Polechtchouk <pavel.overseas@hotmail.com>
2011-01-27 22:47:14 +0100

    added logging config

--- project.clj
+++ project.clj
@@ -1,6 +1,11 @@
 (defproject poseidon-connector "1.0.0-SNAPSHOT"
   :description "A connector between POSEIDON TCP stream and e-Portal DB"
   :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]]
+                 [org.clojure/clojure-contrib "1.2.0"]
+		 [log4j "1.2.15" :exclusions
+		  (javax.mail/mail
+		   javax.jms/jms
+		   com.sun.jdmk/jmxtools
+		   com.sun.jmx/jmxri) ]]
   :dev-dependencies [[swank-clojure "1.2.1"]]
   )

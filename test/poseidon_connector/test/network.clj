24ad9af1b91fef06e7ca41a2143b45d7978bb3b2
Pavel Polechtchouk <pavel.overseas@hotmail.com>
2011-02-01 18:09:38 +0100

    refactoring + v1.0.0 release

--- test/poseidon_connector/test/network.clj
+++ test/poseidon_connector/test/network.clj
@@ -1,6 +1,6 @@
 (ns poseidon-connector.test.network
-  (:use [poseidon-connector.network] :reload-all)
-  (:require [poseidon-connector.frame :as frame :only [blocking-pull process-content]])
+  (:use [poseidon.connector.network] :reload-all)
+  (:require [poseidon.connector.frame :as frame :only [blocking-pull process-content]])
   (:use [clojure.test]))
 
 (def frame1 (str (char 1) "!65129405;00000000;000020;2043129405;091218;094557;90;2205;0;0;0;091218;094644000;0;0;999;0;0;04;0;0;0;0;0;0;0;;1;0;0;0088;1;0;;;;;07;$UTS1;10.100.103.23;DIAG;                                                                                                                                 "))
